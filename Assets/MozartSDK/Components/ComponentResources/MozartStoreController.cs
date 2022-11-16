1eabc75f5a7cfac2eee39b81345c3a13695dac06
btaller <btaller@gmail.com>
2022-11-16 11:52:22 +0000

    Added kart code to repo'

---
+++ Assets/MozartSDK/Components/ComponentResources/MozartStoreController.cs
@@ -0,0 +1,28 @@
+﻿namespace Mozart
+{ 
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    public class MozartStoreController : MozartInventoryController
+    {
+
+        protected override void MozartOnEnable()
+        {
+            if (!GetManager().IsLoggedIn()) return;
+            GetManager().onStoreLoadedEvent += StoreLoaded;
+            GetManager().LoadStore();
+        }
+
+        protected override void MozartOnDisable()
+        {
+            GetManager().onStoreLoadedEvent -= StoreLoaded;
+        }
+
+        private void StoreLoaded()
+        {
+            items = GetManager().storeItems;
+            scroller.DataChanged();
+        }
+    }
+}
