928c1721fc1f185d5c66c3190c50e9cebac9f931
btaller <btaller@gmail.com>
2023-01-16 16:14:37 +0100

    Trying Application.ExternalCall to trigger js for version update

--- Assets/KartRemoteConfig.cs
+++ Assets/KartRemoteConfig.cs
@@ -23,6 +23,7 @@ public class KartRemoteConfig : MozartBehaviorBase
     void Start()
     {
         Debug.Log("VERSION:" + Application.version);
+        Application.ExternalCall("PlayerLoaded", null);
         string configUrl = "https://raw.githubusercontent.com/mozart-xyz/mzkart/main/kart_remote_config.json";
         if (float.Parse(Application.version) < 1f)
         {
